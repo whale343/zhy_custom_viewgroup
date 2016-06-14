524108022a9d213e25403208f96f5c9df425f6b3
whale-She <she15751@163.com>
2016-06-14 14:29:17 +0800

    test

--- app/src/main/java/com/example/zhy_custom_viewgroup/TestActivity.java
+++ app/src/main/java/com/example/zhy_custom_viewgroup/TestActivity.java
@@ -2,17 +2,23 @@ package com.example.zhy_custom_viewgroup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by Administrator on 2016/6/14.
  */
 public class TestActivity extends Activity {
 
+    private static final String TAG = "TestActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+
+        Log.e(TAG, "onCreate: 1");
 
     }
 }

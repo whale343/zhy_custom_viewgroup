ae6415f7cfd8965531718c8e6bc3c1a2f2b53cef
whale-She <she15751@163.com>
2016-06-14 14:26:57 +0800

    test

---
+++ app/src/main/java/com/example/zhy_custom_viewgroup/TestActivity.java
@@ -0,0 +1,18 @@
+package com.example.zhy_custom_viewgroup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/6/14.
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
